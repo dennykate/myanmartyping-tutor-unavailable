97808d86d3fc5d49a64e225ed35cac4885cf4e6c
Denny_Kate <dennykate22@gmail.com>
2023-02-12 09:13:04 +0630

    add home page

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
 import React from "react";
 import { Route, Routes } from "react-router-dom";
-import { Generator, Lesson } from "./pages";
+import { Generator, Home, Lesson } from "./pages";
 
 const App = () => {
   return (
     <>
       <Routes>
+        <Route path="/" element={<Home />} />
         <Route path="/lesson" element={<Lesson />} />
         <Route path="/generator" element={<Generator />} />
       </Routes>
