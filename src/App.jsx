18852be32599370ddb40b3da905f79eab2c73a84
Denny_Kate <dennykate22@gmail.com>
2023-02-12 11:14:57 +0630

    add vercel.json

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ const App = () => {
   return (
     <>
       <Routes>
-        <Route path="/" element={<Lesson />} />
+        <Route path="/" element={<Home />} />
         <Route path="/lesson" element={<Lesson />} />
         <Route path="/generator" element={<Generator />} />
       </Routes>
